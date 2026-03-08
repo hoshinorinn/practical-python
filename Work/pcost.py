5e3850ee9952f0055e40143dc6b51a83fdc26c8d
hoshinorinn <2838166900@qq.com>
2026-03-08 20:07:50 +0800

    Finished exercise 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,13 @@
 # pcost.py
 #
 # Exercise 1.27
+
+cost = 0
+
+with open(r'D:\practical-python\Work\Data\portfolio.csv', 'r') as f:
+    next(f) # skip the head line of the file
+    for line in f:
+        l = line.split(',')
+        cost += float(l[1]) * float(l[2])
+
+print('Total cost', cost)
