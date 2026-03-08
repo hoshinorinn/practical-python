6dc7a41265cf31572780fdac7b693e981b02debc
hoshinorinn <2838166900@qq.com>
2026-03-08 20:28:47 +0800

    Finished execise 1.30 (turning the pcost.py script into a function)

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,12 +2,17 @@
 #
 # Exercise 1.27
 
-cost = 0
+def portfolio_cost(file_name):
+    '''
+    Reads the portfolio data, and returns the total cost of the portfolio as a float.
+    '''
+    cost = 0
+    with open(f'{file_name}', 'r') as f:
+        next(f)
+        for line in f:
+            l = line.split(',')
+            cost += float(l[1]) * float(l[2])
+    return cost
 
-with open(r'D:\practical-python\Work\Data\portfolio.csv', 'r') as f:
-    next(f) # skip the head line of the file
-    for line in f:
-        l = line.split(',')
-        cost += float(l[1]) * float(l[2])
-
-print('Total cost', cost)
+cost = portfolio_cost('D:\\practical-python\\Work\\Data\\portfolio.csv')
+print('Total cost:', cost)
