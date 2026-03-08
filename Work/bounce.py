3d076d04e779bf37da86299d55683ceb24f2dc67
hoshinorinn <2838166900@qq.com>
2026-03-08 15:51:42 +0800

    finished exercise 1.8

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+
+initial_height = 100
+bounce = 3/5
+
+for i in range(10):
+    height = round(bounce*initial_height, ndigits=4)
+    print(i+1, height)
+    initial_height = height
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,22 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000
+monthly_payment = 2684.11
+interest_rate = 0.05
+total_paid = 0
+month = 0
+
+for i in range(12):
+    principal = principal * (1 + interest_rate/12) - monthly_payment - 1000
+    month += 1
+    total_paid += (monthly_payment + 1000)
+
+while(principal > 0):
+    principal = principal * (1 + interest_rate/12) - monthly_payment
+    month += 1
+    total_paid += monthly_payment
+
+print('Total paid', total_paid)
+print('Months required', month)
