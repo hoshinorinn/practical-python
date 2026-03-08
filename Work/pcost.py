c6c2748a30b2096c93a6309717cf7fb86dcf59a0
hoshinorinn <2838166900@qq.com>
2026-03-08 20:38:35 +0800

    Finished exercise 1.31 (add error handling to pcost.py so the function is more robust)

--- Work/pcost.py
+++ Work/pcost.py
@@ -7,12 +7,19 @@ def portfolio_cost(file_name):
     Reads the portfolio data, and returns the total cost of the portfolio as a float.
     '''
     cost = 0
+    fail = 0
     with open(f'{file_name}', 'r') as f:
         next(f)
         for line in f:
-            l = line.split(',')
-            cost += float(l[1]) * float(l[2])
+            try:
+                l = line.split(',')
+                cost += float(l[1]) * float(l[2])
+            except ValueError:
+                fail +=1
+                print(f'{fail} line(s) failed, total cost can be wrong, check your file!')
+                pass
     return cost
 
-cost = portfolio_cost('D:\\practical-python\\Work\\Data\\portfolio.csv')
+# cost = portfolio_cost('D:\\practical-python\\Work\\Data\\portfolio.csv')
+cost = portfolio_cost('D:\\practical-python\\Work\\Data\\missing.csv')
 print('Total cost:', cost)
