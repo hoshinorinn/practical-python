ad5460abf28acf12580eb903d04312c828b02ce4
hoshinorinn <2838166900@qq.com>
2026-03-09 08:58:08 +0800

    Finished exercise 2.4

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,18 @@
 # report.py
 #
 # Exercise 2.4
+
+def read_portfolio(filename):
+    '''
+    Read the portfolio file and return a list of tuples containing the contents of the file.
+    '''
+    res = []
+
+    with open(f'{filename}', 'r') as file:
+        next(file)
+        for line in file:
+            l = line.split(',')
+            tup = (l[0], float(l[1]), float(l[2]))
+            res.append(tup)
+
+    return res
