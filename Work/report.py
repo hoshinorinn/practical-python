d980c15e3838c271e65cca0e4eb9fedd8bf6f930
hoshinorinn <2838166900@qq.com>
2026-03-09 09:23:58 +0800

    Finished exercise 2.6 (add read_prices() to report.py)

--- Work/report.py
+++ Work/report.py
@@ -2,6 +2,8 @@
 #
 # Exercise 2.4
 
+import csv
+
 def read_portfolio(filename):
     '''
     Read the portfolio file and return a list of tuples containing the contents of the file.
@@ -14,5 +16,22 @@ def read_portfolio(filename):
             l = line.split(',')
             dic = {'name': l[0], 'shares': float(l[1]), 'price': float(l[2])}
             res.append(dic)
+
+    return res
+
+def read_prices(filename):
+    '''
+    Read the name-price file into a dictionary.
+    '''
+    res = {}
+
+    with open(filename, 'r') as file:
+        rows = csv.reader(file)
+        for row in rows:
+            try:
+                res[f'{row[0]}'] = float(row[1])
+            except (ValueError, IndexError):
+                print('Oops, there may be a blank line in your file... check it!')
+                pass
 
     return res
