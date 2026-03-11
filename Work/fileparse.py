29dda167beeff6516835857618f2c88f2d9d7c83
hoshinorinn <2838166900@qq.com>
2026-03-11 16:50:10 +0800

    Finished exercise 3.10 (add error control)

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -4,13 +4,17 @@
 
 import csv
 
-def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=',') -> list:
+def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=',', silence_errors=False) -> list:
     '''
     Parse a CSV file into a list of records
     '''
+    if select and not has_headers:
+        raise RuntimeError('select requires column headers')
+
     with open(filename) as f:
         rows = csv.reader(f, delimiter=delimiter)
         records = []
+        row_num = 0
 
         # Read the file headers
         if has_headers:
@@ -25,6 +29,8 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','
                 indices = []
 
             for row in rows:
+                row_num += 1
+                
                 if not row:    # Skip rows with no data
                     continue
                 # Filter the row if specific columns were selected
@@ -33,7 +39,13 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','
 
                 # Convert the types
                 if types:
-                    row = [func(val) for func, val in zip(types, row)]
+                    try:
+                        row = [func(val) for func, val in zip(types, row)]
+                    except ValueError as e:
+                        if not silence_errors:
+                            print(f'Row {row_num}: Couldn\'t convert {row}')
+                            print(f'Row {row_num}:', e)
+                        continue
 
                 # Make a dictionary
                 record = dict(zip(headers, row))
@@ -43,7 +55,13 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','
                 if not row:
                     continue
                 if types:
-                    row = [func(val) for func, val in zip(types, row)]
+                    try:
+                        row = [func(val) for func, val in zip(types, row)]
+                    except ValueError as e:
+                        if not silence_errors:
+                            print(f'Row {row_num}: Couldn\'t convert {row}')
+                            print(f'Row {row_num}:', 'Reason', e)
+                        continue
                 record = tuple(row)
                 records.append(record)
 
