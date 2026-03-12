70332934655869e9ae16f34a05398451687d4765
hoshinorinn <2838166900@qq.com>
2026-03-12 10:49:17 +0800

    Finished exercise 3.12 and 3.14 (using modules implemented previously)

--- Work/pcost.py
+++ Work/pcost.py
@@ -3,26 +3,15 @@
 # Exercise 1.27
 
 import csv
+import report
 
 def portfolio_cost(file_name):
     '''
-    Reads the portfolio data, and returns the total cost of the portfolio as a float.
+    Computes the total cost (shares*price) of a portfolio file.
     '''
-    total_cost = 0
-    with open(f'{file_name}', 'r') as f:
-        rows = csv.reader(f)
-        headers = next(rows)
-        for i, line in enumerate(rows):
-            record = dict(zip(headers, line))
-            try:
-                nshares = int(record['shares'])
-                price = float(record['price'])
-                total_cost += nshares * price
-            except ValueError:
-                print(f'Row {i+1}: Couldn\'t convert: {line}')
-                pass
-    print('Total cost:', total_cost)
-    return
+    portfolio = report.read_portfolio(file_name)
+    return sum([s['shares'] * s['price'] for s in portfolio])
 
-# portfolio_cost('D:\\practical-python\\Work\\Data\\missing.csv')
-portfolio_cost(r'D:\practical-python\Work\Data\portfoliodate.csv')
+# filename = input('Enter the full path of your file: ')
+# cost = portfolio_cost(filename)
+# print('Total cost:', cost)
--- Work/report.py
+++ Work/report.py
@@ -3,39 +3,19 @@
 # Exercise 2.4
 
 import csv
+import fileparse
 
 def read_portfolio(filename):
     '''
     Read the portfolio file and return a list of tuples containing the contents of the file.
     '''
-    res = []
-
-    with open(f'{filename}', 'r') as file:
-        rows = csv.reader(file)
-        head = next(rows)
-        for row in rows:
-            record = dict(zip(head, row))
-            dic = {'name': record['name'], 'shares': int(record['shares']), 'price': float(record['price'])}
-            res.append(dic)            
-
-    return res
+    return fileparse.parse_csv(filename, select=['name','shares','price'], types=[str,int,float])
 
 def read_prices(filename):
     '''
     Read the name-price file into a dictionary.
     '''
-    res = {}
-
-    with open(filename, 'r') as file:
-        rows = csv.reader(file)
-        for row in rows:
-            try:
-                res[f'{row[0]}'] = float(row[1])
-            except (ValueError, IndexError):
-                print('Oops, there may be a blank line in your file... check it!')
-                pass
-
-    return res
+    return dict(fileparse.parse_csv(filename,types=[str,float], has_headers=False))
 
 def make_report(portfolio: list, prices: dict) -> list:
     '''
